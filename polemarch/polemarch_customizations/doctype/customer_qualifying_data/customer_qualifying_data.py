4d99376765aeb00935268716ec7cdb406658125e
suparikoli <manoj311093@gmail.com>
2025-09-11 15:25:59 +0530

    psh
    couple of changes

---
+++ polemarch/polemarch_customizations/doctype/customer_qualifying_data/customer_qualifying_data.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Mithtech Innovative Solutions PVT LTD and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class CustomerQualifyingData(Document):
+	pass
---
+++ polemarch/polemarch_customizations/doctype/customer_qualifying_data/test_customer_qualifying_data.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Mithtech Innovative Solutions PVT LTD and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCustomerQualifyingData(FrappeTestCase):
+	pass
