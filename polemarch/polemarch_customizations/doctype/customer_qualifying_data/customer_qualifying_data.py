ce2dfebea5b1724326a2c69c84b9c723e78879f8
suparikoli <manoj311093@gmail.com>
2025-09-16 13:58:18 +0530

    minorchanges

--- polemarch/polemarch_customizations/doctype/customer_qualifying_data/customer_qualifying_data.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Mithtech Innovative Solutions PVT LTD and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class CustomerQualifyingData(Document):
-	pass
