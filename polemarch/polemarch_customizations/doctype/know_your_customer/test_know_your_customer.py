5bd81b24820f5b00c870a72dbc73ddcc4dcf970c
suparikoli <manoj311093@gmail.com>
2025-11-07 16:44:22 +0530

    changes to polemarch
    remove KYC added it directly to customer

--- polemarch/polemarch_customizations/doctype/know_your_customer/test_know_your_customer.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Mithtech Innovative Solutions PVT LTD and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestKnowYourCustomer(FrappeTestCase):
-	pass
